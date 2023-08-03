62d8d57d5a5a082549fcdfc42b884d2ba8f3c7bb
ufarwa <110379925+ufarwa@users.noreply.github.com>
2023-08-03 18:34:45 +0500

    add some tail blocks
    add some tail blocks in which own CSS and JS is added, also added some transition

---
+++ app.js
@@ -0,0 +1,6 @@
+/* tailbox 1 text area */
+document.addEventListener('DOMContentLoaded', function () {
+    const textContainer = document.getElementById('text-container');
+    textContainer.classList.add('show');
+  });
+  
