87dea94265617da2473e689d1c9f75cd1b5848b1
ufarwa <110379925+ufarwa@users.noreply.github.com>
2023-08-04 18:31:20 +0500

    Home page is done

--- app.js
+++ app.js
@@ -4,3 +4,49 @@ document.addEventListener('DOMContentLoaded', function () {
     textContainer.classList.add('show');
   });
   
+
+// box 3 started
+
+let isAnimating = false;
+
+    function animateCounting(targetValue, duration, percentageValue) {
+      const startValue = parseInt(percentageValue.textContent);
+      const fps = 30; // Frames per second
+      const frameDuration = 1000 / fps;
+      const totalFrames = Math.round(duration / frameDuration);
+      let currentFrame = 0;
+
+      function updateCount() {
+        const progress = currentFrame / totalFrames;
+        const value = Math.round(startValue + (targetValue - startValue) * progress);
+        percentageValue.textContent = value + '%';
+        currentFrame++;
+
+        if (currentFrame <= totalFrames) {
+          requestAnimationFrame(updateCount);
+        }
+      }
+
+      updateCount();
+    }
+
+    function startAnimation(card) {
+      if (!isAnimating) {
+        isAnimating = true;
+        const percentageValue = card.querySelector('.title-font.text-3xl.text-gray-900');
+        const currentValue = parseInt(percentageValue.textContent);
+
+        // Reset the percentage value to 0% on hover
+        percentageValue.textContent = '0%';
+
+        // Start the animation from 0% to the original value (e.g., 45%)
+        animateCounting(currentValue, 2000, percentageValue); // Change the duration as per your preference
+
+        // Delay the reset of isAnimating to prevent starting the animation multiple times
+        setTimeout(() => {
+          isAnimating = false;
+        }, 2000); // Adjust the timeout to match the animation duration
+      }
+    }
+
+    // box 3 ended
